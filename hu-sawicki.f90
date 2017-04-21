dbb90c52e72bc792113c0c92e6f8f7e9e0707810
Andrei Frolov <frolov@sfu.ca>
2017-04-20 18:01:44 -0700

    Typo in DDV fixed, local variables optimized.

--- hu-sawicki.f90
+++ hu-sawicki.f90
@@ -23,13 +23,13 @@ elemental function curvature(phi)
 	if (mu > 0.0 .and. phi > 0.0) then
 		x = ((phi/2.0)**(n+1) + upsilon**(n+1))**(1.0/(n+1))/mu
 	else
-		x = ((-(alpha*n/beta**2)/(phi-2.0*upsilon))**(n/(n+1.0)) - 1.0/beta)**(1.0/n)
+		x = ((-(n*alpha/beta**2)/(phi-2.0*upsilon))**(n/(n+1.0)) - 1.0/beta)**(1.0/n)
 	end if
 	
 	! Newton's method
 	do i = 1,16
-		p = beta*x**n + 1.0; q = x**(n-2); s = p*p
-		x = x + ((phi - 2.0*mu*x)*s + (n*alpha)*q*x)/(2.0*mu*s + (n*alpha)*q*(n+1 - 2*n/p))
+		p = beta*x**n + 1.0; q = (n*alpha)*x**(n-2); s = p*p
+		x = x + ((phi - 2.0*mu*x)*s + q*x)/(2.0*mu*s + q*(n+1-2*n/p))
 	end do
 	
 	curvature = x
@@ -47,8 +47,8 @@ end function DV
 elemental function DDV(phi)
         real DDV, x, p, q, s, phi; intent(in) :: phi
         
-        x = curvature(phi); p = beta*x**n + 1.0; q = x**(n-2); s = p*p
-        DDV = (R0/3.0) * (s - (n*alpha)*q*x*(n+2-2*n/p))/(2.0*mu*s + (n*alpha)*q*(n+2-2*n/p))
+        x = curvature(phi); p = beta*x**n + 1.0; q = (n*alpha)*x**(n-2); s = p*p
+        DDV = (R0/3.0) * (s - q*x*(n+2-2*n/p))/(2.0*mu*s + q*(n+1-2*n/p))
 end function DDV
 
 end module
